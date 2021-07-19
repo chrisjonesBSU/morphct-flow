3a702763289d6ef92bf6e62cc83f1835352dd6b3
Jenny Fothergill <jennyfothergill@u.boisestate.edu>
2021-07-19 15:45:54 -0600

    add explanatory comments, rearrange, add section headers

--- src/init.py
+++ src/init.py
@@ -12,34 +12,50 @@ project_name = "morphct"
 
 # Parameters used for generating the morphology
 parameters = {
-    # input structure (path to a gsd file)
+    # Structure and chemistry
+    #------------------------
+    # Input structure (path to a gsd file)
     "input": ["itic-trajectory.gsd"],
 
+    # Index of the frame in the input gsd trajectory to use
+    "frame": [-1],
+
+    # The number of atoms in each molecule
     "mol_length": [186],
 
-    # chromophore specification
+    # Chemistry-specific reorganization energy in eV
+    "reorganization_energy": [0.15],
+
+    # Chromophore specification
+    #--------------------------
+    # Path to a csv file containing the particle indices of the first
+    # chromophore. The molecule length will be used to scale these indices
+    # out to the rest of the system.
     "acceptors": ["itic_all_ids.csv"],
     "donors": [None],
 
+    # If the pyscf calculation errors out due to electron spin, a charge
+    # can be specified here to fix it.
     "acceptor_charge": [0],
     "donor_charge": [0],
 
-    # frame index
-    "frame": [-1],
-
+    # PlanckTon specific conversions
+    #-------------------------------
     # scale needed to convert lengths to Angstrom
     # (from planckton use the ref_distance in job doc)
     "scale": [3.5636],
 
     "forcefield": ["gaff"],
 
-    "reorganization_energy": [0.15],
-
+    # KMC Parameters
+    #---------------
     # Temperatures specified in Kelvin
     "temperature": [300],
 
+    # Carrier lifetimes in seconds
     "lifetimes": [[1e-13,1e-12]],
 
+    # Number of holes (for donor) and/or electrons (for acceptor)
     "n_holes": [0],
     "n_elec": [10],
 }
